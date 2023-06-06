ecc921bd5889706af51057546ab91e898cf6cb8d
viveknhc <viveknhc@gmail.com>
2023-06-06 18:32:54 +0530

    06/06/23

--- assets/java script/script.js
+++
@@ -1,7 +0,0 @@
-var video = document.getElementById("main-video");
-
-video.addEventListener("ended", function() {
-  video.currentTime = 0; // Reset the video to the beginning
-  video.play(); // Start playing the video again
-});
-
